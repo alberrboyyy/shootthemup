495f124328e2a6faef78e2df13cd7ce2e2950976
alberr_b0yyy <albertbraimi@gmail.com>
2025-09-10 16:34:03 +0200

    feature(code): Ajout des mouvements (wop)

--- shootthemup/Drones/Model/Drone.cs
+++ shootthemup/Drones/Model/Drone.cs
@@ -3,19 +3,19 @@
     // Cette partie de la classe Drone définit ce qu'est un drone par un modèle numérique
     public partial class Drone
     {
-        public static readonly int FULLCHARGE = 1000;   // Charge maximale de la batterie
-        private int _charge;                            // La charge actuelle de la batterie
+        public static readonly int _MaxHealth = 3;       // Charge maximale de la batterie
+        private int _health;                            // La charge actuelle de la batterie
         private string _name;                           // Un nom
         private int _x;                                 // Position en X depuis la gauche de l'espace aérien
         private int _y;                                 // Position en Y depuis le haut de l'espace aérien
 
         // Constructeur
-        public Drone(int x, int y, string name)
+        public Drone(int x, int y, string name, int health)
         {
             _x = x;
             _y = y;
             _name = name;
-            _charge = GlobalHelpers.alea.Next(FULLCHARGE); // La charge initiale de la batterie est choisie aléatoirement
+            _health = health;
         }
         public int X { get { return _x;} }
         public int Y { get { return _y;} }
@@ -25,9 +25,7 @@
         // que 'interval' millisecondes se sont écoulées
         public void Update(int interval)
         {
-            _x += 2;                                    // Il s'est déplacé de 2 pixels vers la droite
-            _y += GlobalHelpers.alea.Next(-2, 3);       // Il s'est déplacé d'une valeur aléatoire vers le haut ou le bas
-            _charge--;                                  // Il a dépensé de l'énergie
+
         }
 
     }
